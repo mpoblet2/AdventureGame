935e482186f4faf27d6b61ab349a0e874e8372f8
Ben Kinder <brkinder1@gmail.com>
2016-11-21 11:17:02 -0500

    Added second question

--- src/MainUI.java
+++ src/MainUI.java
@@ -1,7 +1,4 @@
-import controlP5.Button;
-import controlP5.ControlFont;
-import controlP5.ControlP5;
-import controlP5.Textfield;
+import controlP5.*;
 import processing.core.PApplet;
 import processing.core.PFont;
 
@@ -22,8 +19,13 @@ public class MainUI  extends PApplet{
     private Button buttonTwo;
     private Button buttonThree;
     private Button buttonFour;
-    private Textfield question;
+    private Textarea question;
     private PFont font;
+    private int questionNum = 1;
+    private boolean choiceA = false;
+    private boolean choiceB = false;
+    private boolean choiceC = false;
+    private boolean choiceD = false;
 
     public static void main(String[] args) {
         PApplet.main("MainUI");
@@ -42,29 +44,69 @@ public class MainUI  extends PApplet{
 
         cp5 = new ControlP5(this);
 
-        question1();
-    }
-
-    private void question1() {
-        question = cp5.addTextfield("question");
+        question = cp5.addTextarea("question");
         question.setPosition(widthUnit, heightUnit);
         question.setSize(parseInt(7 * widthUnit), parseInt(2 * heightUnit));
-        question.setText("Do you wish to cast a vote?");
         question.setFont(font);
         question.setLabel("");
 
         buttonOne = cp5.addButton("buttonOne");
         buttonOne.setPosition(widthUnit, 5 * heightUnit);
         buttonOne.setSize(buttonWidth, buttonHeight);
-        buttonOne.setLabel("Yes");
 
         buttonTwo = cp5.addButton("buttonTwo");
         buttonTwo.setPosition(3 * widthUnit, 5 * heightUnit);
         buttonTwo.setSize(buttonWidth, buttonHeight);
+
+        buttonThree = cp5.addButton("buttonThree");
+        buttonThree.setPosition(5 * widthUnit, 5 * heightUnit);
+        buttonThree.setSize(buttonWidth, buttonHeight);
+
+        buttonFour = cp5.addButton("buttonFour");
+        buttonFour.setPosition(7 * widthUnit, 5 * heightUnit);
+        buttonFour.setSize(buttonWidth, buttonHeight);
+
+        question1();
+    }
+
+    private void question1() {
+        question.setText("Would you like to cast a vote?");
+
+        buttonOne.setLabel("Yes");
         buttonTwo.setLabel("No");
+        buttonThree.setVisible(false);
+        buttonFour.setVisible(false);
+    }
+
+    private void question2() {
+        question.setText("What is your socioeconomic class?");
+
+        buttonOne.setLabel("Upper");
+        buttonTwo.setLabel("Middle");
+        buttonThree.setVisible(true);
+        buttonThree.setLabel("Lower");
     }
 
     public void draw() {
+        background(0);
+    }
 
+    public void buttonOne() {
+        if (questionNum == 1){
+            questionNum++;
+            question2();
+        }
+    }
+
+    public void buttonTwo() {
+        if (questionNum == 1) {
+            question.setText("You did not vote.");
+        }
+    }
+
+    public void buttonThree() {
+    }
+
+    public void buttonFour() {
     }
 }
